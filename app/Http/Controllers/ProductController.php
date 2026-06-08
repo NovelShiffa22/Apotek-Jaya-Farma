223e082b4528d035cb9fb771a0c429ed9b455b39
Aisya Zahra <aisyazahra1209@gmail.com>
2026-06-08 19:44:54 +0700

    fix: resolve duplicated Category import in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Symptom;
 use App\Models\Product;
-use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
