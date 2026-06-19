2200a8ca60ea43a764d9d04cd1e83c8d498db9c7
dicodella.go <dicodella.go@gmail.com>
2026-06-19 10:27:35 +0700

    fix: resolve registration failure due to phone regex validation split issue

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'phone' => 'required|numeric|digits_between:10,13|regex:/^(08|62)/',
+            'phone' => ['required', 'numeric', 'digits_between:10,13', 'regex:/^(08|62)/'],
             'password' => ['required', 'confirmed', 'min:8'],
         ]);
 
